c70fb1cc33a379db245abea0b28cba43af71fb60
Doye Lim <doye730@email.com>
2026-04-15 10:25:28 +0900

    Initial commit

--- R/run_CTRLseq.R
+++ R/run_CTRLseq.R
@@ -9,13 +9,27 @@
 #' @return DEG result table
 #' @export
 
-`%as%` <- tensorflow::`%as%`
+if (requireNamespace("tensorflow", quietly = TRUE)) {
+  `%as%` <- tensorflow::`%as%`
+}
 
 run_CTRLseq <- function(counts, group, n_factor = 2, beta = 0.1, seed = 123){
+
+  set.seed(seed)
+  Sys.setenv(TF_ENABLE_ONEDNN_OPTS = 0)
 
   if (!requireNamespace("tensorflow", quietly = TRUE)) {
     stop("tensorflow required")
   }
+
+  tensorflow::tf$random$set_seed(seed)
+  reticulate::py_run_string(paste0("import random; random.seed(", seed, ")"))
+
+  library(edgeR)
+  library(keras3)
+  library(tensorflow)
+
+  `%as%` <- tensorflow::`%as%`
 
   obj <- CTRLseq(
     counts = counts,
